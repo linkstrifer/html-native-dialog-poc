42c6f450a5ffc1441781d1453e390959ed0d143d
Juan Chavarro <chavarro@google.com>
2020-06-03 16:33:21 -0500

    rebuild

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -1082,7 +1082,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57570" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58155" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
